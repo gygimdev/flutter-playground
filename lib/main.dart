6d9ea2a7b9f04c4b04194449df70c0e0c363d2f8
GI YONG GIM <gygimdev@GIs-MacBook-Pro.local>
2023-11-29 11:44:22 +0900

    add: stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_playground/navigator-screen/ScreenA.dart';
-import 'package:flutter_playground/navigator-screen/ScreenB.dart';
-import 'package:flutter_playground/navigator-screen/ScreenC.dart';
 
 void main() {
-  return runApp(MyApp());
+  return runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+
+  int counter = 0;
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
 
     return MaterialApp(
-      title: 'navigator-screen',
-      initialRoute: "/",
-      routes: {
-        "/": (context) => ScreenA(),
-        "/b": (context) => ScreenB(),
-        "/c": (context) => ScreenC()
-      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Stateful Widget", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("you have pushed the button this many times:"),
+              Text("$counter"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    child: const Text("+"),
+                    onPressed: () {
+                      setState(() {
+                        counter++;
+                      });
+                    },
+                  ),
+                  FloatingActionButton(
+                    child: const Text("-"),
+                    onPressed: () {
+                      setState(() {
+                        counter--;
+                      });
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
