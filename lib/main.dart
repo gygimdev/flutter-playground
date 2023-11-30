09743e9b8494248410b4669786a681c8727d6ea2
GI YONG GIM <gygimdev@GIs-MacBook-Pro.local>
2023-11-30 14:45:53 +0900

    playground

--- lib/main.dart
+++ lib/main.dart
@@ -4,59 +4,25 @@ void main() {
   return runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
-
-  int counter = 0;
-
-  @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Stateful Widget", style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("you have pushed the button this many times:"),
-              Text("$counter"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FloatingActionButton(
-                    child: const Text("+"),
-                    onPressed: () {
-                      setState(() {
-                        counter++;
-                      });
-                    },
-                  ),
-                  FloatingActionButton(
-                    child: const Text("-"),
-                    onPressed: () {
-                      setState(() {
-                        counter--;
-                      });
-                    },
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+      title: "master",
+      home: MainPage(),
     );
   }
 }
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+
